a93d26a4a52625ecefcb045b5b01668fcb12eaa8
tatianasanabria21 <karentatianasanabriagongora21@gmail.com>
2024-08-06 14:47:31 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,9 @@
-
+<?php
+$identificacion= $_POST ['identificacion'];
+$NomApellidos= $_POST ['NomApellidos'];
+$Cargo= $_POST ['Cargo'];
+$Salario= $_POST ['Salario'];
+echo "La identifiacion del Empleado es: ".identificacion;
+echo "Los nombres y apellidos del empleado son:  ".NomApellidos;
+echo "El cargo del empleado es:  ".Cargo;
+echo "El salario del empleado es:  ".Salario;
